ac769381b8245b2b1d3d56867b27c874c32c0ea0
zakuraevs <zakuraevs@gmail.com>
2020-11-27 18:46:29 +0200

    Added WIP ranking and changing of ranking for games

--- pipeline-frontend/src/components/GameForm.js
+++ pipeline-frontend/src/components/GameForm.js
@@ -1,15 +1,19 @@
-import React, {useRef} from 'react';
+import React, {useState, useRef} from 'react';
 const { uuid } = require('uuidv4');
 
 const GameForm = ({games, setGames}) => {
 
   const newGameRef = useRef()
+  //const [highestIndex, setHighestindex] = useState(0)
 
   const handleGameSubmit = (event) => {
     const newGameTitle = newGameRef.current.value
+    const newIndex = games.length
+    console.log("Adding a game at index " + newIndex)
     if(newGameTitle === '') return
     setGames(prevGames => {
-      return [...prevGames, {id: uuid(), title: newGameTitle, status: 'not completed'}]
+      //trash below with games.length?
+      return [...prevGames, {id: uuid(), title: newGameTitle, status: 'not completed', rank: newIndex}]
     })
     newGameRef.current.value = null
   }
@@ -18,8 +22,49 @@ const GameForm = ({games, setGames}) => {
     const oldGames =[...games]
     const newGames = oldGames.filter(g => g.id !== id)
     setGames(newGames)
+
   }
 
+  // improve below 2 functions!
+  const increaseRank = (id) => {
+    const oldGames =[...games]
+    // this is verbose
+    const increasingGame = oldGames.find(g => g.id === id)
+    if(increasingGame.rank === 0) return
+
+    const decreasingRank = increasingGame.rank-1
+
+    const decreasingGame = oldGames.find(g => g.rank === decreasingRank)
+
+    increasingGame.rank -= 1
+    decreasingGame.rank += 1
+
+    const newGamesWithDecreased = oldGames.map(g => g.rank === decreasingRank ? decreasingGame : g)
+    const newGamesWithIncreased = newGamesWithDecreased.map(g => g.id === id ? increasingGame : g)
+    
+    setGames(newGamesWithIncreased)
+  }
+
+  const decreaseRank = (id) => {}
+    /*const oldGames =[...games]
+    // this is verbose
+    const decreasingGame = oldGames.find(g => g.id === id)
+    if(decreasingGame.rank > games.length) return
+
+    const decreasingRank = increasingGame.rank-1
+
+    const decreasingGame = oldGames.find(g => g.rank === decreasingRank)
+
+    increasingGame.rank -= 1
+    decreasingGame.rank += 1
+
+    const newGamesWithDecreased = oldGames.map(g => g.rank === decreasingRank ? decreasingGame : g)
+    const newGamesWithIncreased = newGamesWithDecreased.map(g => g.id === id ? increasingGame : g)
+    
+    setGames(newGamesWithIncreased)
+  }*/
+
+  //const sortedByRank = games.sort((a, b) => (a.rank < b.rank) ? -1 : 1)
 
   return (
     <>
@@ -29,7 +74,12 @@ const GameForm = ({games, setGames}) => {
       <ul>
         {games.map(game => 
           <div key={game.id}>
-            <li>{game.title} - {game.status}<button onClick={() => handleDelete(game.id)}>Remove</button></li>
+            <li>
+              {game.title} - {game.status}
+              <button onClick={() => handleDelete(game.id)}>Remove</button>
+              <button onClick={() => increaseRank(game.id)}>↑</button>
+              <button onClick={() => decreaseRank(game.id)}>↓</button>
+            </li>
           </div>
         )}
       </ul>
