255a4008f13f78f5f1670a3ff04f5e145509e1bf
zakuraevs <zakuraevs@gmail.com>
2020-11-28 16:55:10 +0200

    WIP css layout. Going to use CSS grid

--- pipeline-frontend/src/components/Home.js
+++ pipeline-frontend/src/components/Home.js
@@ -21,6 +21,7 @@ const Home = () => {
   const s3 = "ᛗ"
   const s4 = "ᛇ"
   const s5 = "ᚩ"
+  const s6 = "x"
   const imgSource = "https://res.cloudinary.com/teepublic/image/private/s--N4TpEgst--/c_crop,x_10,y_10/c_fit,w_830/c_crop,g_north_west,h_1038,w_1038,x_-104,y_-240/l_upload:v1565806151:production:blanks:vdbwo35fw6qtflw9kezw/fl_layer_apply,g_north_west,x_-215,y_-351/b_rgb:ffb81c/c_limit,f_jpg,h_630,q_90,w_630/v1559034692/production/designs/4942585_0.jpg"
 
   const hard_games = [{name: "Minecraft ", rank: 1, id: 0, description: "Minecraft is a sandbox video game developed by Mojang. The game was created by Markus \"Notch\" Persson in the Java programming language."},
@@ -37,17 +38,21 @@ const Home = () => {
 
   return (
     <>
-      <h1 className="mainHeader">
+      {/*<header className="mainHeader">
           Pipeline
-      </h1>
-      <div className="gamesContainer">
-        {sorted_games.map(
-          game => 
-            <div>
-            <h2 key={game.id} className="singleGame">{game.name}<img className="singleGameImage" src={imgSource}></img></h2>
-            </div>
-        )}
+      </header>*/}
+      <div class="mainContainer">
+        <div class="gamesContainer">
+          {sorted_games.map(
+            game => 
+              <h2 key={game.id} class="singleGame">{game.name} </h2>
+          )}
+        </div>
+        <div class="infoContainer">
+            <p>test</p>
+        </div>
       </div>
+      
     </>
   );
 }
